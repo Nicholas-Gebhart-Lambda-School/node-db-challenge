069626ae73c58038b6ca3366bcd94eae40c946ba
gebhartn <nicholas.gebhart@gmail.com>
2019-11-08 08:42:49 -0800

    feat: add task seed

--- data/seeds/02-tasks.js
+++ data/seeds/02-tasks.js
@@ -1,7 +1,21 @@
 exports.seed = function(knex) {
   return knex("tasks").insert([
-    { id: 1, colName: "rowValue1" },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" }
+    {
+      project_id: 1,
+      task_description: "squish internal organs",
+      table_notes: "yuh"
+    },
+    { project_id: 1, task_description: "squish stomach" },
+    { project_id: 1, task_description: "equip hornace" },
+    {
+      project_id: 2,
+      task_description: "add windex to water",
+      table_notes: "clean"
+    },
+    { project_id: 2, task_description: "wipe the desk with the towels" },
+    { project_id: 2, task_description: "celebrate" },
+    { project_id: 3, task_description: "grind coffee", table_notes: "coffee" },
+    { project_id: 3, task_description: "add water" },
+    { project_id: 3, task_description: "bloom, brew, drink" }
   ]);
 };
