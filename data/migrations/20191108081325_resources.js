cfc3433b1642dd075ceb5ac23342835855586905
gebhartn <nicholas.gebhart@gmail.com>
2019-11-08 08:25:01 -0800

    feat: add resources migration

--- data/migrations/20191108081325_resources.js
+++ data/migrations/20191108081325_resources.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("resources", table => {
+    table.increments();
+    table.string("resource_name", 255).notNullable();
+    table.string("resource_description", 255);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("resources");
 };
