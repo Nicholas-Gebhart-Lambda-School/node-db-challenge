a6fd9cc964534b535c7a8b28c9ca9deadf06f759
gebhartn <nicholas.gebhart@gmail.com>
2019-11-08 08:39:15 -0800

    feat: add resource seed

--- data/seeds/03-resources.js
+++ data/seeds/03-resources.js
@@ -1,7 +1,9 @@
 exports.seed = function(knex) {
   return knex("resources").insert([
-    { id: 1, colName: "rowValue1" },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" }
+    { resource_name: "coffee" },
+    { resource_name: "water" },
+    { resource_name: "tha hornace of death" },
+    { resource_name: "windex" },
+    { resource_name: "paper towels" }
   ]);
 };
