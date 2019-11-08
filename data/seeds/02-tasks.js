729ef9c3f1651fdceae8ea02275f643d78264629
gebhartn <nicholas.gebhart@gmail.com>
2019-11-08 08:46:27 -0800

    chore: run seed

--- data/seeds/02-tasks.js
+++ data/seeds/02-tasks.js
@@ -3,18 +3,18 @@ exports.seed = function(knex) {
     {
       project_id: 1,
       task_description: "squish internal organs",
-      table_notes: "yuh"
+      task_notes: "yuh"
     },
     { project_id: 1, task_description: "squish stomach" },
     { project_id: 1, task_description: "equip hornace" },
     {
       project_id: 2,
       task_description: "add windex to water",
-      table_notes: "clean"
+      task_notes: "clean"
     },
     { project_id: 2, task_description: "wipe the desk with the towels" },
     { project_id: 2, task_description: "celebrate" },
-    { project_id: 3, task_description: "grind coffee", table_notes: "coffee" },
+    { project_id: 3, task_description: "grind coffee", task_notes: "coffee" },
     { project_id: 3, task_description: "add water" },
     { project_id: 3, task_description: "bloom, brew, drink" }
   ]);
