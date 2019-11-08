fcda5dc356c85b9cb4f7641078e3178bd2a08dd7
gebhartn <nicholas.gebhart@gmail.com>
2019-11-08 08:30:41 -0800

    feat: add shared migration

--- data/migrations/20191108081409_shared.js
+++ data/migrations/20191108081409_shared.js
@@ -1,3 +1,22 @@
-exports.up = function(knex) {};
+exports.up = function(knex) {
+  return knex.schema.createTable("shared", table => {
+    table.increments();
+    table
+      .integer("project_id", 64)
+      .unsigned()
+      .references("id")
+      .inTable("projects")
+      .notNullable();
 
-exports.down = function(knex) {};
+    table
+      .integer("resource_id", 64)
+      .unsigned()
+      .references("id")
+      .inTable("resources")
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("shared");
+};
